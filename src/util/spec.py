447c91a198c0609c36586b74b5552b9541c7f89d
Bill Han <brownbear23@naver.com>
2025-04-21 03:09:43 +0000

    feat: torch and cuda version checker added

---
+++ src/util/spec.py
@@ -0,0 +1,12 @@
+import torch
+
+print("PyTorch version:", torch.__version__)
+print("CUDA available:", torch.cuda.is_available())
+print("CUDA version:", torch.version.cuda)
+
+# /workspace/cv-eval/venv/bin/python /workspace/cv-eval/src/util/spec.py
+# PyTorch version: 2.6.0+cu124
+# CUDA available: True
+# CUDA version: 12.4
+#
+# Process finished with exit code 0
